fe18df29c7f8cd370174ad02ff26aee8eb37e89c
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-10-04 14:35:07 +0100

    testing merge by chiboy

--- src/favour/views/pharmacistFolder/pharmacistViewRecordsDashboard.jsx
+++ src/favour/views/pharmacistFolder/pharmacistViewRecordsDashboard.jsx
@@ -5,6 +5,7 @@ import {useNavigate} from "react-router-dom";
 export const PharmacistViewRecordsDashboard =()=>{
     const viewlog = [
         {
+            
             Date: 'Favour chiemela',
             Name: 'Consultation',
             Result: '22/12/2023',
