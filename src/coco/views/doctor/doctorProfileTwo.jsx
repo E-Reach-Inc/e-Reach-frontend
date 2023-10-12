191dd464a7f2a9da38a756ccd05fa8c9dc4f505e
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-10-12 15:28:11 +0100

    comms

--- src/coco/views/doctor/doctorProfileTwo.jsx
+++ src/coco/views/doctor/doctorProfileTwo.jsx
@@ -42,15 +42,15 @@ export const DoctorProfileTwo =()=>{
                         </div>
 
                         <div className="doc-bio-input-dash">
-                            <h3>Full Name:{doctorData.firstName}{doctorData.lastName}</h3>
+                            <h3>Full Name:{"Alayande Abdulmalik"}{doctorData.lastName}</h3>
                             <div className= 'doc-profile-role-text'>
                                 <p>Role: {doctorData.role}</p>
                             </div>
                             <div className= 'doc-profile-email-text'>
-                                <p>Email: {doctorData.email}</p>
+                                <p>Email: {"alaabdulmalik03@gmail.com"}</p>
                             </div>
                             <div className= 'doc-profile-phone-text'>
-                                <p>Phone Number:{doctorData.phoneNumber}</p>
+                                <p>Phone Number:{"09087653412"}</p>
                             </div>
                             <div className= 'doc-profile-docId-text'>
                                 <p>Doctor id: {doctorData.doctorId}</p>
