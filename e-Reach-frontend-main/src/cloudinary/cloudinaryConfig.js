03ce17c00c6f23d948112e098d90c037047ed00b
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-10-12 14:50:46 +0100

    comms

--- e-Reach-frontend-main/src/cloudinary/cloudinaryConfig.js
+++
@@ -1,9 +0,0 @@
-import cloudinary from 'cloudinary-core';
-
-const cloudinaryConfig = {
-    cloud_name: process.env.cloud_name,
-    api_key: process.env.api_key,
-    api_secret: process.env.api_secret,
-};
-
-const cl = new cloudinary.Cloudinary(cloudinaryConfig);
