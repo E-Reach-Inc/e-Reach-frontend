abff31a9439965368d1c486e7187087d1695256a
Akaabiam Richards <richardsakaabiam@gmail.com>
2023-10-13 13:10:10 +0100

    pulled from main

--- src/ritch/views/PatientPopUp.jsx
+++ src/ritch/views/PatientPopUp.jsx
@@ -2,15 +2,33 @@ import React from 'react'
 import '../styles/PatientPopUp.css'
 
 
-const PatientPopUp = ({onClose, isModalOpen}) => {
+const PatientPopUp = (props) => {
+  console.log("pop up value --> ", props.lastTimeUpdated)
+  // const {hospitalEmail, lastTimeUpdated, patientIdentificationNumber} =  props.patientDetail
+  /*
 
+date
+: 
+"12:00:00"
+hospitalEmail
+: 
+"Eexampl@email.come"
+lastTimeUpdated
+: 
+"12:00:00"
+patientIdentificationNumber
+: 
+  */
 
   return (
     <div className='patientPopUp'>
             <div className="patient-inner-pop-up">
-               
+              <p>date: {props.date}</p>
+              <p>hospitalEmail: {props.hospitalEmail}</p>
+              <p>lastTimeUpdated: {props.lastTimeUpdated}</p>
+              <p>patientIdentificationNumber: {props.patientIdentificationNumber}</p>
             </div>
-      <button className='patient-close-button' onClick={onClose}>Close</button>
+      <button className='patient-close-button' onClick={props.onClose}>Close</button>
     </div>
   )
 }
--- src/ritch/views/PatientRecord.jsx
+++ src/ritch/views/PatientRecord.jsx
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from "react";
 import PatientNavBar from "./PatientNavBar";
-import action from '../patient-icons/patient-eye-view.svg'
-import '../styles/PatientRecord.css'
+import action from "../patient-icons/patient-eye-view.svg";
+import "../styles/PatientRecord.css";
 import PatientSideBar from "./PatientSideBar";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import PatientPopUp from "./PatientPopUp";
 
-
-
-const PatientRecord = () =>{
-
+const PatientRecord = () => {
   const [buttonPopUp, setButtonPopUp] = useState(false);
-  const [selectedMedicalLog, setSelectedMedicalLog] = useState(null);
+  const [selectedMedicalLog, setSelectedMedicalLog] = useState();
 
   const openPopup = (medicalLog) => {
+    console.log("valuePatient value --> ", medicalLog.lastTimeUpdated);
     setSelectedMedicalLog(medicalLog);
     setButtonPopUp(true);
   };
@@ -24,126 +22,140 @@ const PatientRecord = () =>{
     setButtonPopUp(false);
   };
 
-
-    const medicalLogs = [
-        {
-            date: "",
-            // lastTimeUpdated: '12:00:00',
-            // patientIdentificationNumber: 'e123456789990',
-            hospitalEmail: ''
-            // action: {action},
-        },
-        {
-          date: "",
-          // lastTimeUpdated: '12:00:00',
-          // patientIdentificationNumber: 'e123456789990',
-          hospitalEmail: ''
-          // action: {action},
-      },
-      {
-        date: "",
-        // lastTimeUpdated: '12:00:00',
-        // patientIdentificationNumber: 'e123456789990',
-        hospitalEmail: '',
-       action: "",
+  const medicalLogs = [
+    {
+      date: "12:00:00",
+      lastTimeUpdated: "12:00:00",
+      patientIdentificationNumber: "e123456789990",
+      hospitalEmail: "Eexampl@email.come",
+      // action: {action},
     },
-        
-    ];
-    useEffect(()=>{
-    const patientIdentificationNumber = localStorage.getItem("patientIdentificationNumber")
-    const hospitalEmail = localStorage.getItem("hospitalEmail")
-    console.log("hi pin")
-    console.log("hi email")
+    {
+      date: "12:00:00",
+      lastTimeUpdated: "12:00:00",
+      patientIdentificationNumber: "e123456789990",
+      hospitalEmail: "Eexampl@email.come",
+      // action: {action},
+    },
+    {
+      date: "12:00:00",
+      lastTimeUpdated: "12:00:00",
+      patientIdentificationNumber: "e123456789990",
+      hospitalEmail: "Eexampl@email.come",
+      action: "",
+    },
+  ];
+  useEffect(() => {
+    const patientIdentificationNumber = localStorage.getItem(
+      "patientIdentificationNumber"
+    );
+    const hospitalEmail = localStorage.getItem("hospitalEmail");
+    console.log("hi pin");
+    console.log("hi email");
 
-    if(patientIdentificationNumber){
-
-        axios.get(" http://e-reach-prod.up.railway.app/api/v1/patient/view-records/"+patientIdentificationNumber)
-            .then(response => {
-                if(!response.data)
-                  toast.info("No records found", {position: toast.POSITION.TOP_CENTER, autoClose: 5000})
-                else {
-                    medicalLogs.push(response.data)
-                    localStorage.setItem("records", JSON.stringify(response.data));
-
-                }
-            })
-            .catch(failureResponse => {
-
-            })
-            .finally()
+    if (patientIdentificationNumber) {
+      axios
+        .get(
+          " http://e-reach-prod.up.railway.app/api/v1/patient/view-records/" +
+            patientIdentificationNumber
+        )
+        .then((response) => {
+          console.log(response)
+          if (!response.data)
+            toast.info("No records found", {
+              position: toast.POSITION.TOP_CENTER,
+              autoClose: 5000,
+            });
+          else {
+            console.log("response.data ==> ", response.data);
+            medicalLogs.push(response.data);
+            localStorage.setItem("records", JSON.stringify(response.data));
+          }
+        })
+        .catch((failureResponse) => {})
+        .finally();
+    } else if (hospitalEmail) {
+      axios
+        .get(
+          "http://e-reach-prod.up.railway.app/api/v1/hospital/view-records/" +
+            hospitalEmail
+        )
+        .then((response) => {
+          if (!response.data)
+            toast.info("No records found", {
+              position: toast.POSITION.TOP_CENTER,
+              autoClose: 5000,
+            });
+          else {
+            console.log("Record --> ", response.data);
+            medicalLogs.push(response.data);
+            localStorage.setItem("records", JSON.stringify(response.data));
+          }
+        })
+        .catch((failureResponse) => {})
+        .finally();
     }
+  }, []);
+  return (
+    <div className="patient-record-outter-div">
+      <ToastContainer />
+      <div>
+        <PatientSideBar />
+      </div>
 
-    else if(hospitalEmail){
-        axios.get("http://e-reach-prod.up.railway.app/api/v1/hospital/view-records/"+hospitalEmail)
-            .then(response => {
-              if(!response.data)
-                toast.info("No records found", {position: toast.POSITION.TOP_CENTER, autoClose: 5000})
-              else {
-                  medicalLogs.push(response.data)
-                  localStorage.setItem("records", JSON.stringify(response.data));
-
-              }
-            })
-            .catch(failureResponse => {
-
-            })
-            .finally() 
-    }
-}, [])
-    return(
-        <div className="patient-record-outter-div">
-            <ToastContainer/>
-            <div>
-                <PatientSideBar/> 
-            </div>
-           
-           <div className="patient-record-inner-div">
-              <div>
-              <PatientNavBar/>
-              </div>
-
-              <div  className="patient-record-inner-con">
-
-             
-    <h3 className="patient-med-logs">Medical logs</h3>
-        <table className="patient-record-table">
-
-          <thead className="patient-record-table-header">
-            <tr>
-              <th>Date Created</th>
-              {/* <th>Last Time Updated</th>
-              <th>Patient Identification Number</th> */}
-              <th>Hospital Email</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody className="patient-record-table-data">               
-                {medicalLogs.map((medicalLogs, index) => (
-                                <tr key={index}>
-                                    <td >{medicalLogs.date}</td>
-                                    {/* <td >{patientsRecords.lastTimeUpdated}</td>
-                                    <td >{patientsRecords.patientIdentificationNumber}</td> */}
-                                    <td>{medicalLogs.hospitalEmail}</td>
-                                    <td onClick={() => openPopup(medicalLogs)}>
-                                      {medicalLogs.action} <img src={action} alt="Open Popup" />
-                                        </td>
-                                </tr>
-                  ))}
-              </tbody>
-    
-              {buttonPopUp && (
-        <PatientPopUp onClose={closePopup} isModalOpen={true} medicalLog={selectedMedicalLog} />
-      )}
-         
-          </table>
-          {buttonPopUp && <PatientPopUp onClose={closePopup} isModalOpen={true}/>}
+      <div className="patient-record-inner-div">
+        <div>
+          <PatientNavBar />
         </div>
-           
-    
-        </div>     
-        
+
+        <div className="patient-record-inner-con">
+          <h3 className="patient-med-logs">Medical logs</h3>
+          <table className="patient-record-table">
+            <thead className="patient-record-table-header">
+              <tr>
+                <th>Date Created</th>
+                {/* <th>Last Time Updated</th>
+              <th>Patient Identification Number</th> */}
+                <th>Hospital Email</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody className="patient-record-table-data">
+              {medicalLogs.map((value, index) => (
+                <tr key={index}>
+                  <td>{value.date}</td>
+                  {/* <td >{patientsRecords.lastTimeUpdated}</td>
+                                    <td >{patientsRecords.patientIdentificationNumber}</td> */}
+                  <td>{value.hospitalEmail}</td>
+                  <td onClick={() => openPopup(value)}>
+                    {value.action} <img src={action} alt="Open Popup" />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+
+            {buttonPopUp && (
+              <PatientPopUp
+                onClose={closePopup}
+                isModalOpen={true}
+                pat={{ name: "Hello" }}
+              />
+            )}
+          </table>
+          {buttonPopUp && (
+            <PatientPopUp
+              onClose={closePopup}
+              isModalOpen={true}
+              lastTimeUpdated={selectedMedicalLog.lastTimeUpdated}
+              hospitalEmail={selectedMedicalLog.hospitalEmail}
+              patientIdentificationNumber={selectedMedicalLog.patientIdentificationNumber}
+              date={selectedMedicalLog.date}
+              // patientDetail={selectedMedicalLog}
+            />
+          )}
+        </div>
+      </div>
     </div>
-    )
-    
-}
+  );
+};
 export default PatientRecord;
